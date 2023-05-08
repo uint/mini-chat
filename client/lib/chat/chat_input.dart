615de682f180393251753466ae36f8f0b6520ef5
Tomasz Kurcz <uint@users.noreply.github.com>
2023-05-09 00:38:38 +0200

    mock message sending

--- client/lib/chat/chat_input.dart
+++ client/lib/chat/chat_input.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:minichat_client/chat_repo.dart';
 
-class ChatInput extends StatelessWidget {
+class ChatInput extends ConsumerWidget {
   const ChatInput({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder(
-      fallbackHeight: 40,
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FloatingActionButton(onPressed: () {
+      var repo = ref.read(chatRepositoryProvider);
+      repo.pushMessage(Message(DateTime.now(), User("me"), "hi"));
+    });
   }
 }
